e69e89f4fd25e9259c754d2cf828c80b4e552c4c
danwakeem <danwakeem@gmail.com>
2019-05-28 16:39:54 -0500

    Linted

--- index.spec.js
+++ index.spec.js
@@ -1,53 +1,55 @@
-const proxyquire = require('proxyquire');
-const sandbox = require('sinon').createSandbox();
+const proxyquire = require("proxyquire");
+const sandbox = require("sinon").createSandbox();
 
-describe('index', () => {
-  let index, rollbar, errorSpy;
+describe("index", () => {
+  let index;
+  let rollbar;
+  let errorSpy;
   beforeEach(() => {
     errorSpy = sandbox.spy();
-    rollbar = sandbox.stub().returns({error: errorSpy});
-    index = proxyquire('./index', {
-      rollbar,
+    rollbar = sandbox.stub().returns({ error: errorSpy });
+    index = proxyquire("./index", {
+      rollbar
     });
   });
 
   afterEach(() => sandbox.restore());
 
-  it('should return a function', () => {
+  it("should return a function", () => {
     const res = index({});
     res.should.be.a.Function();
   });
 
-  it('should init rollbar with ROLLBAR_ACCESS_TOKEN', () => {
-    process.env.ROLLBAR_ACCESS_TOKEN = 'abc';
+  it("should init rollbar with ROLLBAR_ACCESS_TOKEN", () => {
+    process.env.ROLLBAR_ACCESS_TOKEN = "abc";
     const res = index({});
-    
+
     sandbox.assert.calledOnce(rollbar);
     sandbox.assert.calledWith(rollbar, {
       verbose: true,
       captureUncaught: true,
       captureUnhandledRejections: true,
-      accessToken: 'abc',
+      accessToken: "abc"
     });
   });
 
-  describe('return function', () => {
-    it('should call rollbar.error', () => {
+  describe("return function", () => {
+    it("should call rollbar.error", () => {
       const res = index({});
       const next = sandbox.spy();
-      res({error: true}, {}, {}, next);
+      res({ error: true }, {}, {}, next);
 
       sandbox.assert.calledOnce(errorSpy);
-      sandbox.assert.calledWith(errorSpy, {error: true});
+      sandbox.assert.calledWith(errorSpy, { error: true });
     });
 
-    it('should call next', () => {
+    it("should call next", () => {
       const res = index({});
       const next = sandbox.spy();
-      res({error: true}, {}, {}, next);
+      res({ error: true }, {}, {}, next);
 
       sandbox.assert.calledOnce(next);
-      sandbox.assert.calledWith(next, {error: true});
+      sandbox.assert.calledWith(next, { error: true });
     });
   });
 });
